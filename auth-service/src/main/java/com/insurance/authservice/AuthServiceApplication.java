a00e0ecee7efe31521ff714a7f0f0e823b02ee74
Devanshu <devanshu2343@gmail.com>
2026-03-30 16:15:22 +0530

    Git Commit

--- auth-service/src/main/java/com/insurance/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/insurance/authservice/AuthServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 }
